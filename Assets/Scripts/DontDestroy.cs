590e387db59ac3d340dffbfaf26b647c70a7e84e
Ian Jones <ian@DN0a1f6a35.SUNet>
2018-10-14 18:14:27 -0700

    first commit

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1) 
+            Destroy(objs[0]);
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,24 @@ public class PlayerMovement : MonoBehaviour {
 
 	// This is a reference to the Rigidbody component called "rb"
 	public Rigidbody rb;
+    public CharacterController c;
 
-	public float forwardForce = 2000f;	// Variable that determines the forward force
-	public float sidewaysForce = 500f;  // Variable that determines the sideways force
+	public float forwardForce;	// Forward movement speed
+	public float sidewaysForce;  // Sideways movement speed
+    public float upwardsForce;   // Jumping
+
+    private bool onGround = true;
 
 	// We marked this as "Fixed"Update because we
 	// are using it to mess with physics.
 	void FixedUpdate ()
 	{
-		// Add a forward force
-		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        rb.freezeRotation = true;
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
 		if (Input.GetKey("d"))	// If the player is pressing the "d" key
-		{
+        {
 			// Add a force to the right
 			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
@@ -27,9 +32,26 @@ public class PlayerMovement : MonoBehaviour {
 			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
+        if (Input.GetKey("w"))
+        {
+            if (onGround)
+            {
+                rb.AddForce(0, upwardsForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+                onGround = false;
+            }
+        }
+
 		if (rb.position.y < -1f)
 		{
 			FindObjectOfType<GameManager>().EndGame();
 		}
 	}
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Ground")) 
+        {
+            onGround = true;
+        }
+    }
 }
